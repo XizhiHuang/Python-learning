2e3c5b4a919840936b84e41fac6ff67a8f3fc149
XizhiHuang <332888191@qq.com>
2021-03-25 21:08:55 +0800

    补充3.3节

--- pytorch实战/3.3. 线性回归简化.py
+++ pytorch实战/3.3. 线性回归简化.py
@@ -1,41 +1,9 @@
 import torch
-from time import time
-
-"""
-
-a = torch.ones(1000)
-b = torch.ones(1000)
-
-start = time()
-# 作为标量对两个向量进行计算
-c = torch.zeros(1000)
-for i in range(1000):
-    c[i] = a[1] + b[i]
-end = time()
-print(end - start)
-
-start_d = time()
-# 直接对两个向量进行计算
-d = a + b
-end_d = time()
-print(start_d - end_d)
-
-"""
-
-"""
-
-# 简单张量计算
-a = torch.ones(3)
-b = 10
-print(a + b)
-
-"""
-
-import torch
 from IPython import display
 from matplotlib import pyplot as plt
 import numpy as np
 import random
+import torch.utils.data as Data
 
 num_inputs = 2
 num_examples = 1000
@@ -66,23 +34,32 @@ plt.rcParams['figure.figsize'] = (5.0, 5.0)
 plt.scatter(features[:, 1].numpy(), labels.numpy(), 20)
 plt.show()
 
+batch_size = 10
 
 # 读取数据  每次返回batch_size（批量大小）个随机样本的特征和标签。
-def read_data(batch_size, features, labels):
-    num_examples = len(features)
-    indices = list(range(num_examples))
-    # 随机读取整个样本
-    random.shuffle(indices)
-    for i in range(0, num_examples, batch_size):
-        # 考虑到最后一次取值可能会超出num_examples的范围
-        j = torch.LongTensor(indices[i:min(i + batch_size, num_examples)])
-        # a = features.index_select(0, j)
-        # b = labels.index_select(0, j)
-        yield features.index_select(0, j), labels.index_select(0, j)
-        # return a, b
+dataset = Data.TensorDataset(features, labels)
+# 随机读取小批量数据 shuffle实现
+data_iter = Data.DataLoader(dataset, batch_size, shuffle=True)
+
+for x_mini, y in data_iter:
+    print(x_mini)
+    print(y)
+    break
+
+#class LinearNet(nn.Module):
+    #def __init__(self,n_feature):
 
 
-batch_size = 10
+
+
+
+
+
+
+
+
+
+"""
 # 配合使用yield 可以使得x和y分别得到赋值       也可使用return，但是使用return就只能对x赋值
 for x_mini, y in read_data(batch_size, features, labels):
     print(x_mini)
@@ -99,11 +76,9 @@ b.requires_grad_(requires_grad=True)
 def linreg(x, w, b):
     return torch.mm(x, w) + b
 
-
 # 构建损失函数
 def squared_loss(y_predict, y):
     return (y_predict - y.view(y_predict.size())) ** 2 / 2
-
 
 # 定义小批量随机梯度函数
 def sgd(params, lr, batch_size):
@@ -132,3 +107,4 @@ for epoch in range(num_epochs):
 
     print(true_w, '\n', w)
     print(true_b, '\n', b)
+"""
